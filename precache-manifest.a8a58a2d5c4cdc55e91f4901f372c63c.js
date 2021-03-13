b9966d504617667a19c5dc378f80cc92eb66ed73
mhsidd997 <muhammedhamzasiddiqui@gmail.com>
2021-03-14 00:24:39 +0500

    Updates

---
+++ precache-manifest.a8a58a2d5c4cdc55e91f4901f372c63c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "433e0656ac8ee324332d4f225eddbdd4",
+    "url": "/Leadersboard_React_App/index.html"
+  },
+  {
+    "revision": "0fdd905cbf713d31bb62",
+    "url": "/Leadersboard_React_App/static/css/main.ea54f82e.chunk.css"
+  },
+  {
+    "revision": "a097f18292f14344df19",
+    "url": "/Leadersboard_React_App/static/js/2.4dd7275c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Leadersboard_React_App/static/js/2.4dd7275c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fdd905cbf713d31bb62",
+    "url": "/Leadersboard_React_App/static/js/main.75d4b82e.chunk.js"
+  },
+  {
+    "revision": "754316bf907b4b453ea1",
+    "url": "/Leadersboard_React_App/static/js/runtime-main.71565384.js"
+  }
+]);
